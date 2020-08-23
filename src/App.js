17876d6cfcf3be024fa34c2979bacb05fdef78fc
“Jhoancanchila” <canchila.jhoan@gmail.com>
2020-08-22 19:36:07 -0500

    [ADD]categories-and-styles

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { ListOfCategories } from './components/listOfCategories'
+import { GlobalStyle } from './GlobalStyles'
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <ListOfCategories />
+    </>
+  )
+}
+
+export default App
